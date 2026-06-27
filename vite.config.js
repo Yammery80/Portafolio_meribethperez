0679908b1ec8867217c5672ddac49a42adab1df5
Yammery80 <yammery.28@gmail.com>
2026-06-26 19:56:57 -0600

    Agregar workflow de GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Portafolio_meribethperez',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
